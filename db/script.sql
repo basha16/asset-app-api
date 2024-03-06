c793b8bfd0b6c9b7c1aad0c1b46de8e293049a1b
ersathbasha <ersathbasha.a@gmail.com>
2024-03-06 17:40:36 +0530

    script

--- db/script.sql
+++ db/script.sql
@@ -1,11 +1,24 @@
 BEGIN;
+
+    CREATE TABLE user (
+        id VARCHAR(36) PRIMARY KEY NOT NULL,
+        first_name VARCHAR(44) NOT NULL,
+        last_name VARCHAR(44),
+        email VARCHAR(100) NOT NULL,
+        position VARCHAR(60) NOT NULL,
+        password VARCHAR(60) NOT NULL,
+        team VARCHAR(60),
+        created_at DATE,
+        updated DATE
+    );
+
     CREATE TABLE user_assets (
         id VARCHAR(36) PRIMARY KEY NOT NULL,
         name VARCHAR(255) NOT NULL,
         description VARCHAR(455),
         quantity INT NOT NULL DEFAULT 0,
         assignee VARCHAR(36) NOT NULL,
-        status ENUM('Active', 'Inactive', 'Pending') DEFAULT 'Active' NOT NULL,
+        status ENUM('Active', 'Returned', 'damaged') DEFAULT 'Active' NOT NULL,
         created_at DATE,
         updated DATE,
         FOREIGN KEY (assignee) REFERENCES user(id)
