f30e48592c41605d7a5ce58f64b6dd1b97fe530a
ersathbasha <ersathbasha.a@gmail.com>
2024-03-07 15:17:09 +0530

    env

--- database.js
+++ database.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql2/promise')
+require('dotenv').config();
 
 const db = mysql.createPool({
     host: "localhost",
-    user:'root',
-    password :'',
-    database:'assest_management',
+    user: 'root',
+    password: '',
+    database: 'assest_management',
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    port: process.env.PORT
 })
 
 module.exports = {
